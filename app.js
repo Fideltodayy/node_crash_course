1d33b70771b372224017823fce9f14e03b68ffe0
Fideltodayy <fidelotieno11@gmail.com>
2023-12-17 14:44:02 +0300

    used ejs to pass and render dynamic data in the blog page and map ever it

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ app.listen(3000);
 
 app.get('/', (req,res)=>{
     console.log("Serving the Home page")
-    res.render('index',{title: "Home"})
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index',{title: "Home",blogs})
 })
 app.get('/about', (req,res)=>{
     res.render("about",{title: "About"})
