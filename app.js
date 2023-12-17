e9f8f2bf2ed2700acb3b6333f37f12e36d6b091f
Fideltodayy <fidelotieno11@gmail.com>
2023-12-17 13:59:51 +0300

    Installed and used ejs to render dynamic data in the title tag

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express')
 
 const app = express(console.log("Server is up and running"));
-
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req,res)=>{
     console.log("Serving the Home page")
-    res.sendFile("./Views/index.html", { root: __dirname })
+    res.render('index',{title: "Home"})
 })
 app.get('/about', (req,res)=>{
-    res.sendFile("./Views/about.html", { root: __dirname })
+    res.render("about",{title: "About"})
+})
+app.get('/blogs/create', (req,res)=>{
+    res.render("create",{title: "Create Blog"})
 })
 app.get('/about_me', (req,res)=>{
     console.log("Redirecting from ",req.url ,"to /about")
-    res.redirect('./about')
+    res.redirect('about')
 })
 app.use((req,res)=>{
-    res.status(404).sendFile("./Views/404.html", { root: __dirname })
+    res.status(404).render("404",{title: "404"});
 })
